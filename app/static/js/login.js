ded0f12413b3d882e207907658d959398e843ac1
Randy Deng <ryangdeng@gmail.com>
2018-04-16 00:14:18 -0400

    Removed unnecessary js files and ensured usage of https in login forms

--- app/static/js/login.js
+++
@@ -1,9 +0,0 @@
-// Toggles view for officer skills areatext
-function toggle_officer_skills() {
-var x = document.getElementById("position")
-	if (x.value === "PI") {
-	  document.getElementById("officer_skills").classList.add("hidden");
-	} else {
-	  document.getElementById("officer_skills").classList.remove("hidden");
-	}
-}
