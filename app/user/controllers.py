d7a3822339bf57deeb42d382a500823f9d7503f1
Randy Deng <ryangdeng@gmail.com>
2018-05-18 23:15:10 -0400

    Fixed routing issues in user blueprint. Return 404 error if user tries to access a page they should not be accessing. Route back to login blueprint if applicant mistakenly tries to access user blueprint.

--- app/user/controllers.py
+++ app/user/controllers.py
@@ -1,24 +1,27 @@
-from flask import redirect, render_template
+from flask import abort, redirect, render_template, url_for
 
-from app.login.user_login_manager import valid_applicant, first_login
+from app.login.user_login_manager import valid_applicant
 
 from . import user
 
 
 @user.route('/<string:id>', methods=['GET'])
+def route(id):
+    if valid_applicant(id, flashing=False):
+        return redirect(url_for('.dashboard', id=id))
+    else:
+        return redirect(url_for('login.route', id=id))
+
+
 @user.route('/dashboard/<string:id>', methods=['GET'])
 def dashboard(id):
-    if first_login(id, flashing=False):
-        return redirect('/login/' + id)
-    if not valid_applicant(id, flashing=True):
-        return redirect('/login/user')
+    if not valid_applicant(id, flashing=False):
+        abort(404)
     return render_template('user_dashboard.html', user_id=id)
 
 
 @user.route('/interviews/<string:id>', methods=['GET'])
 def documentation(id):
-    if first_login(id, flashing=False):
-        return redirect('/login/' + id)
-    if not valid_applicant(id, flashing=True):
-        return redirect('/login/user')
+    if not valid_applicant(id, flashing=False):
+        abort(404)
     return render_template('user_interviews.html', user_id=id)
