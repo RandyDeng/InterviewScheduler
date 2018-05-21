58fda7e35d80934ed8b37cd72a125ec36bf84edb
Randy Deng <ryangdeng@gmail.com>
2018-05-21 01:24:35 -0400

    Return a single error page for all errors

--- app/__init__.py
+++ app/__init__.py
@@ -21,23 +21,11 @@ def home():
 
 
 @app.errorhandler(400)
-def bad_request(error):
-    return render_template('errors/400.html'), 400
-
-
 @app.errorhandler(404)
-def not_found(error):
-    return render_template('errors/404.html'), 404
-
-
 @app.errorhandler(500)
-def internal_service(error):
-    return render_template('errors/500.html'), 500
-
-
 @app.errorhandler(503)
-def service_unavailable(error):
-    return render_template('errors/503.html'), 503
+def error(error):
+    return render_template('errors/404.html'), 404
 
 
 app.register_blueprint(login_module, url_prefix='/login')
