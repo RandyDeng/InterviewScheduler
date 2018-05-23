2c1f6add72e229917165487dc997a03f88a04763
Randy Deng <ryangdeng@gmail.com>
2018-05-23 00:10:37 -0400

    Moved semester start dates slightly earlier and added documentation

--- app/utils/environment.py
+++ app/utils/environment.py
@@ -2,7 +2,11 @@ import datetime
 import os
 
 
-# Environment variables saved on Heroku
+# Environmental variables are stored inside a .env file for
+# testing locally and on Heroku for staging and production.
+# These variables contain extremely sensitive information regarding
+# the project and should not be disclosed to anyone except
+# the developers working on this project.
 variables = {
   'BASE_URL': os.environ.get('BASE_URL'),
   'CSRF_SESSION_KEY': os.environ.get('CSRF_SESSION_KEY'),
@@ -13,9 +17,12 @@ variables = {
 }
 
 
-# Dictionary of Available Positions
-# Map: <key=abbreviation, value=position name>
-# Not all positions will be available every semester
+# The following is a dictionary of all the available
+# positions in The Hive. Each position is matched with its
+# abbreviated version. Not all positions will be available
+# at all times and some may not go through the Interview Scheduler
+# process.
+# Currently, only PI and the core officer positions are supported.
 positions = {
     'MPI': 'Master Peer Instructor',
     'PI': 'Peer Instructor',
@@ -32,13 +39,17 @@ positions = {
 }
 
 
-# Threshold values are hard-coded to determine Fall, Spring, Summer semester
-fall_start = {'month': 8, 'day': 20}
-spring_start = {'month': 1, 'day': 5}
-summer_start = {'month': 5, 'day': 15}
+# These variables determine the start and end of each school semester.
+# The starting dates are put slightly before the first day of class
+# since most of The Hive hiring happens at the beginning of the semester
+# rather than at the end.
+fall_start = {'month': 8, 'day': 15}
+spring_start = {'month': 1, 'day': 1}
+summer_start = {'month': 5, 'day': 10}
 
 
-# Determines the current semester
+# Determines the current semester.
+# The semester start and end dates are hardcoded.
 def current_semester():
     now = datetime.date.today()
     fall_begin = datetime.date(year=now.year,
@@ -61,7 +72,9 @@ def current_semester():
     return "NA"
 
 
-# Determines what the next 3 semesters are
+# Determines the next 3 semesters.
+# This is mainly used in the Applicant form to determine which
+# semesters an applicant will be on campus.
 def next_semesters():
     current = current_semester().split()
     next_year = str(int(current[1]) + 1)
